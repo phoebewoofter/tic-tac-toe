93a7f1cb12b28a605ee3be152b18a5146b2272db
pwoofter <phoebewoofter@gmail.com>
2025-06-09 19:50:33 -0700

    Adds welcome content and button that then renders the game upon clicking play

--- src/App.js
+++ src/App.js
@@ -21,11 +21,21 @@ function PlayAgain({ onButtonClick }) {
     );
   }
 
+  function TicTacToe({ pageLoad, play }) {
+    return (
+      <div onLoad={pageLoad}>
+        <h1><span>Tic</span> <span></span>Tac<span> </span>Toe</h1>
+
+        <button onClick={play}>Play</button>
+      </div>
+    );
+  }
 
 
 export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isClicked, setIsClicked] = useState(false);
 
 
   function handleClick(i) {
@@ -56,36 +66,46 @@ export default function Board() {
     setSquares(Array(9).fill(null));
   }
 
+  function onPlay() {
+    setIsClicked(true);
+  }
+
   let isNotNull = value => value != null;
   let noWinner = squares.filter(isNotNull);
 
 
   return (
     <>
-      <div className="status">{ noWinner.length === 9 ? <p className="status">No winner</p> : status }</div>
+      {!isClicked ? (
+      <TicTacToe play={onPlay} />
+       ) : (
+      <div className="board"> 
+        <div className="status">{ (noWinner.length === 9) && (!winner) ? <p className="status">No winner</p> : status }</div>
 
-       <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+        <div className="board-row">
+          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+        </div>
+        <div className="board-row">
+          <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+          <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+          <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+        </div>
+        <div className="board-row">
+          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+          <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+        </div>
+        <div className="play-again">
+          {(calculateWinner(squares)) || (noWinner.length === 9) ?
+          <PlayAgain onButtonClick={reset}/>
+          :
+          <div></div>
+          }
+        </div>
       </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
-      <div className="play-again">
-        {(calculateWinner(squares)) || (noWinner.length === 9) ?
-        <PlayAgain onButtonClick={reset}/>
-        :
-        <div></div>
-        }
-      </div>
+      )}
     </>
   );
 }
